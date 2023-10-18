5f9ef417fb1c2f6dbf793ebae6a25db2baf01391
John Bledsoe <59550+johnbcodes@users.noreply.github.com>
2023-10-18 11:14:16 -0400

    Use ArenaAllocator for easier heap management of zig-sqlite allocated items Simpler insertion with defined struct

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,31 @@
 const std = @import("std");
 const sqlite = @import("sqlite");
 
+const Person = struct {
+    id: usize,
+    age: u32,
+    name: []const u8,
+};
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer if (gpa.deinit() == .leak) {
         std.debug.panic("leaks detected", .{});
     };
 
-    var allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer arena.deinit();
+    var allocator = arena.allocator();
+
+    std.fs.cwd().deleteFile("data.db") catch |e| {
+        switch (e) {
+            error.FileNotFound => {},
+            else => return e,
+        }
+    };
 
     var db = try sqlite.Db.init(.{
-        .mode = sqlite.Db.Mode{ .File = "data.db" },
+        .mode = .{ .File = "data.db" },
         .open_flags = .{ .write = true, .create = true },
     });
     defer db.deinit();
@@ -21,30 +36,23 @@ pub fn main() !void {
     _ = try db.pragma(void, .{}, "foreign_keys", "on");
 
     // Create a table
-    try db.exec("CREATE TABLE user(id integer primary key, age integer, name text)", .{}, .{});
+    try db.exec("CREATE TABLE user(id integer primary key, age integer, name text) strict", .{}, .{});
+
+    var marge = Person{ .id = 1, .age = 34, .name = "Marge" };
+    var homer = Person{ .id = 2, .age = 36, .name = "Homer" };
 
     // Insert some data
     const insert: []const u8 = "INSERT INTO user(id, age, name) VALUES($id{usize}, $age{u32}, $name{[]const u8})";
-    const user_name: []const u8 = "Vincent";
-    try db.exec(insert, .{}, .{ @as(usize, 10), @as(u32, 34), user_name });
-    try db.exec(insert, .{}, .{ @as(usize, 20), @as(u32, 84), @as([]const u8, "José") });
+    try db.exec(insert, .{}, marge);
+    try db.exec(insert, .{}, homer);
 
-    var stmt = try db.prepare("SELECT id, name, age FROM user where id > 99 order by id");
+    var stmt = try db.prepare("SELECT id, name, age FROM user order by id");
     defer stmt.deinit();
 
-    const rows = try stmt.all(
-        struct {
-            id: usize,
-            name: []const u8,
-            age: usize,
-        },
+    _ = try stmt.all(
+        Person,
         allocator,
         .{},
         .{},
     );
-
-    for (rows) |row| {
-        allocator.free(row.name);
-    }
-    allocator.free(rows);
 }
